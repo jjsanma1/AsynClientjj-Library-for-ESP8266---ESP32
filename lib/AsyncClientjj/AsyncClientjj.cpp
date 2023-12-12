1e1331d54fe26f78ca4c1c43aae39c79db571523
jjsanma1 <jjsanma1@gmail.com>
2023-12-12 10:16:32 +0100

    Update

--- lib/AsyncClientjj/AsyncClientjj.cpp
+++ lib/AsyncClientjj/AsyncClientjj.cpp
@@ -1,7 +1,8 @@
 /*
-  AsyncClientjj - Library or ESP8266 and ESP32 that manages Async HTTP requests to any server (IFTTT,etc)
-  It serializes requests to be sent to the server one by one. Max_Requests by default is 5 but it can be modified in the code of this class. 
- * V1.0 . Author: JJ - August 2023. Tested in ESP8266 and ESP32.Arduino IDE 2.1.1. ESP8266 boards 3.1.2 . ESP32 boards 2.0.11
+  AsyncClientjj - Is a Library for ESP8266 and ESP32 that manages Asyncronous GET requests to any server (IFTTT,etc)
+  It can send requests in paralell to several servers , then if you need the response you can get asyncronously.
+  The maximum simultaneous Requests by default is 5 but it can be modified in the code of this class. 
+  V1.0 . Author: JJ - August 2023. Tested in ESP8266 and ESP32.Arduino IDE 2.1.1. ESP8266 boards 3.1.2 . ESP32 boards 2.0.11
   For ESP8266 Uses ESPAsyncTCP :https://github.com/dvarrel/ESPAsyncTCP (V1.1.4. date  2023 august 2)
   For ESP32 Uses   AsyncTCP :https://github.com/dvarrel/AsyncTCP  (V1.2.4. date  2019 )
   
